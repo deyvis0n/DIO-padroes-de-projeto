30ce764534b8078c7d10f896aae66d023ed3e1a9
deyvis0n <deyvisonsferreira@outlook.com>
2022-11-13 19:10:11 -0300

    Primeiro Commit

---
+++ src/main/java/one/digitalinnovation/lpp/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.lpp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+}

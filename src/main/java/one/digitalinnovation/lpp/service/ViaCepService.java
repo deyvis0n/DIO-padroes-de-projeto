ef1eb17f93e95f1927da15e1e21321208c3f8340
deyvis0n <deyvisonsferreira@outlook.com>
2022-11-13 22:43:48 -0300

    FIX EnderecoDTO para EnderecoViaCepDTO

--- src/main/java/one/digitalinnovation/lpp/service/ViaCepService.java
+++ src/main/java/one/digitalinnovation/lpp/service/ViaCepService.java
@@ -1,11 +1,11 @@
 package one.digitalinnovation.lpp.service;
 
-import one.digitalinnovation.lpp.controller.dto.EnderecoDTO;
+import one.digitalinnovation.lpp.controller.dto.EnderecoViaCepDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
   @GetMapping("/{cep}/json/")
-  EnderecoDTO cosultarCep(String cep);
+  EnderecoViaCepDTO cosultarCep(String cep);
 }

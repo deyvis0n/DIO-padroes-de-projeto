4a25a5388f27ab9c873db6de476c0615b07dca67
deyvis0n <deyvisonsferreira@outlook.com>
2022-11-13 23:02:21 -0300

    FIX ViaCepService PathVariable

--- src/main/java/one/digitalinnovation/lpp/service/ViaCepService.java
+++ src/main/java/one/digitalinnovation/lpp/service/ViaCepService.java
@@ -3,9 +3,10 @@ package one.digitalinnovation.lpp.service;
 import one.digitalinnovation.lpp.controller.dto.EnderecoViaCepDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
   @GetMapping("/{cep}/json/")
-  EnderecoViaCepDTO cosultarCep(String cep);
+  EnderecoViaCepDTO cosultarCep(@PathVariable String cep);
 }

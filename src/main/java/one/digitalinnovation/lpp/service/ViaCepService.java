07fca442ebd412d215b81d26deda46120db343da
deyvis0n <deyvisonsferreira@outlook.com>
2022-11-13 22:36:35 -0300

    FEAT implementaçcao de FeignCliente

--- src/main/java/one/digitalinnovation/lpp/service/ViaCepService.java
+++ src/main/java/one/digitalinnovation/lpp/service/ViaCepService.java
@@ -1,7 +1,11 @@
 package one.digitalinnovation.lpp.service;
 
 import one.digitalinnovation.lpp.controller.dto.EnderecoDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
+  @GetMapping("/{cep}/json/")
   EnderecoDTO cosultarCep(String cep);
 }
